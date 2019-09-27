16e56923c657a8f81939c35dd1758a357c8045ea
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2019-09-26 21:12:14 -0500

    Comprobar server sintax

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ require('dotenv').config();
 import app from './app';
 import '@babel/polyfill';
 
-//Método principal que ejecuta la aplicación como servidar
-async function main(){
-    //Configuración del servidor con las variables del entorno
-    await app.listen(app.get('HTTP_PORT'), app.get('SERVER_HOST'));
-    console.log('Web service escuchando por puerto: ' + app.get('SERVER_HOST') + ':' + app.get('HTTP_PORT'));
-};
+const hostname = app.get('SERVER_HOST'); //DIRECCION DEL SERVIDOR
+const port = app.get('HTTP_PORT'); //PUERTO
 
-main();
+app.listen(port, hostname, function () {
+    console.log('Web service escuchando en http://' + hostname + ':' + port);
+});
